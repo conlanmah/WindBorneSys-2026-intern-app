78f1998fd50bdfb6f01c65421c066e564e873aa2
conlanmah <50381040+conlanmah@users.noreply.github.com>
2025-10-11 15:17:56 -0400

    Initial Nix setup

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Simple Django project with Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pythonPackages = ps: with ps; [
+          django
+          gunicorn
+          whitenoise
+          pytest
+          black
+        ];
+        pythonEnv = pkgs.python311.withPackages pythonPackages;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pythonEnv
+            sqlite
+          ];
+          
+          shellHook = ''
+            echo "🚀 Django development environment ready!"
+            echo "Create project: django-admin startproject myproject ."
+            echo "Run migrations: python manage.py migrate"
+            echo "Start server: python manage.py runserver"
+          '';
+        };
+      });
+}
